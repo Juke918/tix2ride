8c81beb7c7b2eacde866f383b336e39a9e771c9e
Juke918 <kennedyj21@bcdschool.org>
2019-05-31 09:12:02 -0400

    Update main.py

--- main.py
+++ main.py
@@ -6,29 +6,29 @@ trains = [12.0, 12.0, 12.0, 12.0, 12.0, 12.0, 12.0, 12.0, 14.0]
 
 your_hand = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
 
-2_hand =
+#2_hand =
 
 pool = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
 
 trash = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
 
-deck = 0.0
+deck_num = 0.0
 
 def deckcheck(tranes, dek):
 	dek = 0.0
-	for ele in range(0, len(trains)): 
-		dek = dek + tranes[ele] 
+	for ele in range(0, len(trains)):
+		dek = dek + tranes[ele]
 	print(dek)
 
 def prob(tranes):
 
-	deck = 0.0
-	for ele in range(0, len(tranes)): 
-		deck = deck + tranes[ele] 
-	print('The probability for each train is:')
+	deck_num = 0.0
+	for ele in range(0, len(tranes)):
+		deck_num = deck_num + tranes[ele]
+	print('The probability for each train being drawn next is:')
 	print('')
 	for x in range(0, 9):
-		like = tranes[x]/deck
+		like = tranes[x]/deck_num
 		print(trains_str[x])
 		print(like)
 		print('')
@@ -71,6 +71,14 @@ def main():
 
 
 
-#deckcheck(trains, deck)
+#deckcheck(trains, deck_num)
 begin()
 #prob(trains)
+
+
+#class player:
+	#pass
+
+#player_list = []
+
+#player_list.append(player("joe", 12, 15))
